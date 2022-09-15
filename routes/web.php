0046cf296b7d3504fa0af6046422cc6159c819cb
Syota Nishimura <syota4723@gmail.com>
2022-09-15 22:11:04 +0900

    :sparkles: ステップ４クリア

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,4 @@ Route::get('practice3', function () {
 Route::get('practice', [PracticeController::class,"sample"]);
 Route::get('practice2', [PracticeController::class,"sample2"]);
 Route::get('practice3', [PracticeController::class,"sample3"]);
+Route::get('getPractice', [PracticeController::class,"getPractice"]);
